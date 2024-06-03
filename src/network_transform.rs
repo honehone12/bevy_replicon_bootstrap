761b4497fc8ddb3a30bc070c8cd6a4bc59cc443f
honehone <honehone.41games@gmail.com>
2024-06-03 14:17:26 +0900

    fixed https://github.com/honehone12/bevy_replicon_action/issues/18#issuecomment-2144249201

--- src/network_transform.rs
+++ src/network_transform.rs
@@ -112,11 +112,8 @@ pub struct NetworkAngle(pub f32);
 impl LinearInterpolatable for NetworkAngle {
     #[inline]
     fn linear_interpolate(&self, rhs: &Self, t: f32) -> Self {
-        let mut delta = (rhs.0 - self.0) % 360.0;
-
-        if delta < -180.0 {
-            delta += 360.0;
-        } else if delta > 180.0 {
+        let mut delta = ((rhs.0 - self.0) + 360.0) % 360.0;
+        if delta > 180.0 {
             delta -= 360.0;
         }
 
